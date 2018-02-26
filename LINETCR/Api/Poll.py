5eb3fa934b7511f7995d9a6f3d13ac6b6d911c9b
Jay <32067621+jayaranah@users.noreply.github.com>
2018-02-26 09:48:17 +0800

    Add files via upload

--- LINETCR/Api/Poll.py
+++ LINETCR/Api/Poll.py
@@ -18,8 +18,8 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/1.7.14"
-  LA = "CHROMEOS\t1.7.14\tChrome_OS\t1"      
+  UA = "Line/6.0.0 iPad4,1 9.0.2"
+  LA = "IOSIPAD\t7.14.0\tiPhone OS\t10.12.0"
 
   rev = 0
 
